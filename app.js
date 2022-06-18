5f6caaa2481b0a38c83af829bd5bdce62f81cef7
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-18 22:55:29 +0100

    Update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash"); // lodash
 
-// lodash
-const _ = require("lodash");
+const app = express();
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -14,10 +14,8 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
+// middlware
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -28,7 +26,7 @@ const newPosts = [];
 app.get(`/`, function (req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
-    posts: newPosts
+    posts: newPosts,
   });
 });
 
@@ -70,14 +68,14 @@ app.get("/posts/:postTitle", (req, res) => {
     if (storedTitle === requestedTitle) {
       res.render("post", {
         postTitle: post.title,
-        postContent: post.content
+        postContent: post.content,
       });
     }
   });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(port || 3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
