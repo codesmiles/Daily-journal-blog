eee705a880d87e328ef5b0b6aa010fbf5d75c4df
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-19 21:44:55 +0100

    compose works to the db

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs");
 const _ = require("lodash"); // lodash
 const mongoose = require("mongoose"); // mongoose
 const app = express();
-const newsLetterSchema = require("./model/schema.js");
+const NewsLetterSchema = require("./model/schema");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -20,19 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // GLOBAL ARRY VARIABLE FOR POSTS
-// const newPosts = []; OLD WAY
+const newPosts = []; //OLD WAY
 
 // SET UP MONGOOSE CONNECTION------------------
 const url = `mongodb://localhost:27017/clientLocation`;
 // connect to mongoose
-mongoose.connect(url, function (err) { 
-  if (err) { console.log(err); }
+mongoose.connect(url, function (err) {
+  if (err) {
+    console.log(err);
+  }
   console.log(`Connected to MongoDB`);
 });
 
-
 // --------------------------------------------
-
 
 // render the eJS home template file
 app.get(`/`, function (req, res) {
@@ -66,21 +66,14 @@ app.post("/compose", (req, res) => {
   // push to new posts
   // newPosts.push(post); THIS IS THE OLD WAY
 
-
   // save to mongoDB
-  const insertNews = newsLetterSchema.create(post);
-  insertNews.save(err,newData => {
+  const insertNews =new NewsLetterSchema(post);
+  insertNews.save((err, newData)=>{
     if(err){
-      console.log(err)
+      console.log(err);
     }
-    res.json({
-      success: true,
-      messag: "Post saved successfully",
-      data: newData,
-      statusCode: 200
-    })
+    console.log(newData);
   });
-
 
   res.redirect("/");
 });
